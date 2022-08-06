a666867aeec58c3016d6fb4ea721ea0b85f4a468
gabrlaur <45432522+gabrlaur@users.noreply.github.com>
2022-08-06 20:43:49 +0300

    Module #4 final task

--- src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/HalfPyramid.java
@@ -2,7 +2,20 @@ package school.mjc.stage0.loops.finalTask;
 
 public class HalfPyramid {
     public void printHalfPyramid(int cathetusLength) {
+        int whitespace = cathetusLength - 1;
+        for (int i = 0; i < cathetusLength; i++) {
+            int numbersToPrint = cathetusLength - whitespace;
+            for (int j = 0; j < whitespace; j++) {
+                System.out.print(" ");
+            }
 
+            for (int j = 0; j < numbersToPrint; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+            whitespace -= 1;
+        }
     }
 
     public static void main(String[] args) {
--- src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
+++ src/main/java/school/mjc/stage0/loops/finalTask/Pyramid.java
@@ -2,10 +2,39 @@ package school.mjc.stage0.loops.finalTask;
 
 public class Pyramid {
     public void printPyramid(int cathetusLength) {
+        int whitespace = cathetusLength - 1;
+        int number = 1;
+        boolean increase = true;
+        for (int i = 0; i < cathetusLength; i++) {
+            int numbersToPrint = cathetusLength - whitespace + i;
+            for (int j = 0; j < whitespace; j++) {
+                System.out.print(" ");
+            }
 
+            for (int j = 0; j < numbersToPrint; j++) {
+                System.out.print(number);
+
+                if (number == i + 1 && j != numbersToPrint-1) {
+                    increase = false;
+                }
+
+                if (number == 1) {
+                    increase = true;
+                }
+
+                if (increase) {
+                    number++;
+                } else {
+                    number--;
+                }
+            }
+
+            System.out.println();
+            whitespace -= 1;
+        }
     }
 
     public static void main(String[] args) {
-        new Pyramid().printPyramid(7);
+        new Pyramid().printPyramid(5);
     }
 }
